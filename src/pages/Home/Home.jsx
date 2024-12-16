ef3d1e9e67c63ced4db4680c44ce2ed58b5a6115
alexcas20 <alexcalvillosalas@gmail.com>
2024-12-15 18:39:58 -0600

    chore: add mui icons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import ButtonAppBar from '../../assets/components/ButtonAppBar'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <ButtonAppBar/>
   )
 }
