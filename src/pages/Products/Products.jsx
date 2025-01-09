b65c1b0a0b6a3933b38c16c16311b91ac2cdcb9f
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-08 22:01:53 -0600

    feat: creation Products page to refactor

---
+++ src/pages/Products/Products.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { ProductsCard } from "../../components/ProductsCard";
+
+export const Products = () => {
+  //Request products
+  const [products, setProducts] = useState([]);
+  const URL = "http://localhost:8080/api/v1/products";
+
+  const getProducts = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <article className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] place-items-center p-4 pt-12 gap-6 md:gap-5 2xl:gap-1 mb-8">
+      {products.map((product) => (
+        <ProductsCard key={product.id} product={product} />
+      ))}
+    </article>
+  );
+};
