1a2edd46efec4a7b5b5a4bd501fcde50ae68acca
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-08 22:08:40 -0600

    feat: add new route to products page

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { Cart } from "../pages/Cart/Cart";
+import { Products } from "../pages/Products/Products";
 import { ResponsiveAppBar } from "../components/ResponsiveAppBar";
 
 export const AppRouter = () => {
   return (
     <>
       <BrowserRouter>
-      <ResponsiveAppBar/>
+        <ResponsiveAppBar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/products" element={<Products />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="*" element={<Home />} />
         </Routes>
