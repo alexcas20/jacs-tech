d92fddc8f15aebf6ceed314b22a873b58721ae18
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-12 22:41:06 -0600

    fix(style): fix some bugs in mobile view in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,10 +7,10 @@ export const Home = () => {
         <img
           src={homeImg}
           alt="Setup Gaming"
-          className="object-cover opacity-95"
+          className="object-cover opacity-95 w-full h-[550px]"
         />
-        <div className="absolute bottom-0  bg-slate-950 w-full h-1/4 p-4 md:p-8 transition-all group-hover:h-[55%] group-hover:py-1 md:group-hover:h-1/3 md:group-hover:p-8">
-          <h3 className="tracking-wide capitalize text-slate-50 text-sm md:text-4xl text-center font-thin transition-all group-hover:opacity-0 md:group-hover:opacity-100 md:group-hover:text-3xl group-hover:text-start ">
+        <div className="absolute bottom-0  bg-slate-950 w-full h-1/4 p-12 md:p-10 transition-all group-hover:h-[35%] group-hover:py-1 md:group-hover:h-1/4 md:group-hover:p-8">
+          <h3 className="tracking-wide capitalize text-slate-50 text-xl md:text-4xl text-center font-thin transition-all group-hover:opacity-0 md:group-hover:opacity-100 md:group-hover:text-3xl group-hover:text-start ">
             The coolest tech store in the world is arrive
           </h3>
           <p className="text-slate-400 text-md md:text-lg font-thin opacity-0 transition-opacity group-hover:opacity-100">
