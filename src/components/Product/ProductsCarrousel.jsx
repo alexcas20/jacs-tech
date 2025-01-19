d508d3f5f1b749a5ebf7047718f9e9071ea2c134
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-19 16:57:37 -0600

    feat: add function to determinate if the view is mobile to change the slides per view of the carrusel

--- src/components/Product/ProductsCarrousel.jsx
+++ src/components/Product/ProductsCarrousel.jsx
@@ -3,11 +3,29 @@ import { useNavigate } from "react-router-dom";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// Is Mobile
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+};
+
 export const ProductsCarrousel = ({ category, id }) => {
   const [productsCat, setProductsCat] = useState([]);
 
   //Navigate
   const navigate = useNavigate();
+
+  const isMobile = useIsMobile();
 
   const URL = "http://localhost:8080/api/v1/products";
 
@@ -27,23 +45,23 @@ export const ProductsCarrousel = ({ category, id }) => {
   }, [category, id]);
 
   return (
-    <div className="">
-      <h2 className="font-semibold text-3xl capitalize tracking-wider py-4">
+    <div className="md:w-[1200px] px-6">
+      <h2 className="font-semibold text-3xl capitalize tracking-wider py-4 text-center">
         You may also like
       </h2>
-      <hr className="border-b-black pb-8" />
+      <hr className="border-b-slate-900 border-b-2  w-[200px] mx-auto mb-8" />
 
       {/*   Swiper */}
-      <div className="flex gap-4 bg-slate-50 rounded-2xl w-[800px]">
+      <div className="flex gap-4 bg-slate-50 rounded-2xl w-[300px] md:w-[780px] lg:w-full">
         <Swiper
           modules={[Navigation, Autoplay]}
           autoplay={{ delay: 2000 }}
-          slidesPerView={2}
+          slidesPerView={isMobile ? 1 : 2}
           spaceBetween={10}
         >
           {productsCat.length > 1 &&
             productsCat.map((product, index) => (
-              <SwiperSlide key={index} className="p-4 ">
+              <SwiperSlide key={index} className="p-4 cursor-pointer">
                 <img
                   src={product.urlImage}
                   alt={product.name}
@@ -53,7 +71,7 @@ export const ProductsCarrousel = ({ category, id }) => {
                   {product.name}
                 </p>
                 <button
-                  className="w-[100%] border border-slate-600 rounded-md py-2 text-slate-800 my-4 transition-all hover:bg-black hover:text-slate-50 hover:scale-105"
+                  className="w-full border border-slate-600 rounded-md py-2 text-slate-800 my-4 transition-all hover:bg-black hover:text-slate-50 hover:scale-105"
                   onClick={() => navigate(`/product/${product.id}`)}
                 >
                   Buy Now
