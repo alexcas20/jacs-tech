62873262f78a98194d9f607d261d4168756d398c
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-24 11:28:25 -0600

    feat: added new animation to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ export default {
       fontFamily: {
         sans: ['Roboto', 'sans-serif'], 
     },
+    animation: {
+      beat: "beat 1s",
+    },
+    keyframes: {
+      beat: {
+        "0%": { transform: "scale(1)" },
+        "25%": {transform: "scale(1.3)"},
+        "50%": {transform: "scale(1)"},
+        "75%": {transform: "scale(1.3)"},
+        "100%": { transform: "scale(1)" },
+      },
+    }
   },
   },
   plugins: [],
