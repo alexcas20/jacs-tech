d0d541fcbef9f0f68b9054edd3ab7c33e41385fd
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-26 15:48:07 -0600

    fix(reducer): fix action to do when adding a product that already is in favorites

--- src/features/favSlice.js
+++ src/features/favSlice.js
@@ -15,17 +15,20 @@ const favSlice = createSlice({
         (item) => item.id === action.payload.id
       );
 
-      console.log(state.favItems)
+      console.log(state.favItems);
 
       existingItem
-        ? alert("It's already in favs")
+        ? (state.favItems = state.favItems.filter(
+            (item) => item.id !== action.payload.id
+          ))
         : state.favItems.push(action.payload);
     },
 
     deleteFav: (state, action) => {
-
-      console.log(action.payload)
-      state.favItems = state.favItems.filter((item) => item.id != action.payload);
+      console.log(action.payload);
+      state.favItems = state.favItems.filter(
+        (item) => item.id != action.payload
+      );
     },
   },
 });
