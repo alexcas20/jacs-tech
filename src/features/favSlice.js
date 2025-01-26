bc0eb4ad998a924b865c82ba3edef4815ae5d77a
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-26 15:01:30 -0600

    fix(reducer): fix error to delete a product

--- src/features/favSlice.js
+++ src/features/favSlice.js
@@ -15,22 +15,17 @@ const favSlice = createSlice({
         (item) => item.id === action.payload.id
       );
 
+      console.log(state.favItems)
+
       existingItem
         ? alert("It's already in favs")
         : state.favItems.push(action.payload);
     },
 
     deleteFav: (state, action) => {
-      const existingItem = state.favItems.find(
-        (item) => item.id === action.payload.id
-      );
 
-      if (existingItem) {
-        const favsFilter = state.favItems.filter(
-          (item) => item.id !== action.payload.id
-        );
-        state.favItems.push(favsFilter);
-      } else alert("Error, id not found");
+      console.log(action.payload)
+      state.favItems = state.favItems.filter((item) => item.id != action.payload);
     },
   },
 });
