6b2f525c8dfb133b15f6fcc52f7ab87b1bf1f0cc
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-26 15:04:36 -0600

    refactor: favorites component restructuring - update styles - add svg icon to show when the favorites list is empty

--- src/pages/Favs/Favorites.jsx
+++ src/pages/Favs/Favorites.jsx
@@ -1,40 +1,98 @@
+import { ArrowLeftIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteFav } from "../../features/favSlice";
+import { useNavigate } from "react-router-dom";
 
-const favorites = [
-    {id: 1, name: "thing 1", price: 550.2},
-    {id: 3, name: "thing 2", price: 550.2},
-    {id: 4, name: "thing 4", price: 550.2},
-
-]
-
+import NotFavs from "../../assets/icons/sad_favs.svg";
 
 export const Favorites = () => {
+  // get data from reducer
+  const itemsFav = useSelector((state) => state.fav.favItems);
+
+  // get reducer actions
+  const dispatch = useDispatch();
+
+  // delete of fav items
+  const handleDeleteFav = ({ id }) => {
+    if (id !== undefined || null) dispatch(deleteFav(id));
+    else alert("id not found");
+  };
+
+  // navigate
+  const navigate = useNavigate();
+
   return (
-    <article className="container mx-auto min-h-screen">
-
-        <div className="mb-14">
-        <h3 className="text-3xl font-bold pt-12">My Favorites</h3>
-        <hr className="border border-b-black w-[40%] my-2" />
-        </div>
-       
-      
-      <div className="flex gap-4 flex-wrap justify-center mb-10">
-      {
-        favorites.map(fav => 
-            <div key={fav.id} className="bg-slate-200 w-[350px] h-[400px] p-6">
-                <div className="bg-slate-50 rounded-full h-10">
-                    Hola
-                </div>
-                <div>
-                    <span>{fav.name}</span>
-                    <span>{fav.price}</span>
-                </div>
-            </div>
-        )
-      }
+    <article className="min-h-screen flex flex-col items-center md:flex-row md:px-8 md:justify-evenly">
+      <div className="flex justify-center pt-10 md:h-screen md:items-center ">
+        <h2 className="text-2xl md:text-[40px] pl-2 my-2 border-l-4  font-sans font-bold border-blue-700  text-slate-950 sticky top-[40%]">
+          My Favorites
+        </h2>
       </div>
-      
+      <hr className="my-4 border w-60 md:hidden" />
 
+      {itemsFav.length ? (
+        <div className="border mb-8 rounded-md mt-12 w-[90%] lg:w-[55%]">
+          <h3 className="bg-slate-100 text-xl p-3">Favorites List</h3>
+          <hr />
 
+          {itemsFav.map((item, i) => (
+            <div
+              key={i}
+              className="px-6 py-2 relative cursor-pointer h-[260px]"
+            >
+              <div className="flex flex-col items-center gap-4 group ">
+                <img
+                  className="w-36 h-30 object-cover"
+                  src={item.urlImage}
+                  alt={`Imgage of ${item.name}`}
+                />
+                <div className="text-center flex flex-col -mt-3 w-full ">
+                  {/* About product */}
+                  <span className="font-semibold">{item.name}</span>
+                  <span className="tracking-widest text-slate-800">
+                    ${item.price}
+                  </span>
+
+                  {/*   Delete of fav list */}
+                  <div className="absolute top-3 right-4">
+                    <button onClick={() => handleDeleteFav(item)}>
+                      <XMarkIcon className="w-6" />
+                    </button>
+                  </div>
+
+                  {/* Buy now Hover */}
+                  <div className="w-[100%] flex justify-center">
+                    <button
+                      className="p-2 w-[60%] transition-all duration-500 translate-y-12 opacity-0 bg-slate-900 text-slate-50 rounded-lg group-hover:opacity-100 group-hover:translate-y-0 group-hover:mt-1 hover:bg-slate-700"
+                      onClick={() => navigate(`/product/${item.id}`)}
+                    >
+                      Buy Now
+                    </button>
+                  </div>
+                </div>
+                <hr className="my-0 w-[100%] group-hover:opacity-0" />
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        // No favs products
+        <div className="flex flex-col items-center gap-8 cursor-pointer group">
+          <img
+            className="w-52 pt-10 invert-0"
+            src={NotFavs}
+            alt="There aren't any product"
+          />
+          <p className="text-lg tracking-wider text-slate-600">
+            There aren't any products added to favorites yet!
+          </p>
+          <button className="text-slate-50 bg-slate-950 px-4 py-2 rounded-lg flex gap-2 items-center shadow-md transition-all duration-300 opacity-0 translate-y-80 hover:bg-slate-800 group-hover:translate-y-0 group-hover:opacity-100 "
+          onClick={() => navigate("/products")}>
+            <ArrowLeftIcon className="w-5" />
+            Back To Shop
+          </button>
+        </div>
+      )}
     </article>
   );
 };
