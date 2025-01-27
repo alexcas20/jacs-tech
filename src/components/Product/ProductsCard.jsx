9e663bc607d8aa6db6554231723b0c0744bf754c
alexcas20 <alexcalvillosalas@gmail.com>
2025-01-27 16:26:45 -0600

    style: update styles to responsive view

--- src/components/Product/ProductsCard.jsx
+++ src/components/Product/ProductsCard.jsx
@@ -6,7 +6,7 @@ export const ProductsCard = ({ product }) => {
 
   return (
     <>
-      <div className=" bg-slate-100 rounded-xl pb-5 w-[340px] h-[420px] 2xl:h-[450px] cursor-pointer shadow-lg shadow-slate-300 transition-all hover:scale-105">
+      <div className=" bg-slate-100 rounded-xl pb-5 w-[320px] h-[420px] md:w-[250px] 2xl:w-[420px] 2xl:h-[450px] cursor-pointer shadow-md shadow-slate-300 transition-all hover:scale-105">
         <div className="flex items-center justify-center h-56 ">
           <img
             className="w-full h-full object-cover"
@@ -32,9 +32,9 @@ export const ProductsCard = ({ product }) => {
           </div>
 
           {/* Actions */}
-          <div className="pt-4 w-[300px] mx-auto">
+          <div className="pt-4 flex justify-center">
             <button
-              className="bg-slate-950 w-full text-slate-50 py-2 px-2 text-sm rounded-md transition-all font-medium hover:-translate-y-1 hover:bg-slate-700"
+              className="bg-slate-950 w-[100%] text-slate-50 py-2 px-2 text-sm rounded-md transition-all font-medium hover:-translate-y-1 hover:bg-slate-700"
               onClick={() => navigate(`/product/${product.id}`)}
             >
               Buy Now
